8f077dceeede435c706faf1098d3f499a0cd491d
kcromanpl <kcromanpl@gmail.com>
2019-09-18 19:23:29 +0545

    Database connection initialized with mysql2

---
+++ database_connect.rb
@@ -0,0 +1,11 @@
+require 'mysql2'
+require 'pry'
+
+
+class Database
+
+  def self.create_con
+    Mysql2::Client.new(:host => "localhost", :username => "Roman", :password => "Roman123", :database => "attnd_db")
+  end
+  
+end
