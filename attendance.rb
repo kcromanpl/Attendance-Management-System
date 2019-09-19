38d611604e07e82237af94c6d25d58be02c20fec
kcromanpl <kcromanpl@gmail.com>
2019-09-19 07:50:04 +0545

    Database tables added to database_connect.rb

--- attendance.rb
+++ attendance.rb
@@ -8,34 +8,34 @@ class Attendance
      begin
        @client =   Database.create_con
        @empclass = Employee.new(@client)
-       #table - employees 
-       @client.query("
-         CREATE TABLE IF NOT EXISTS employees(
-         emp_id INT PRIMARY KEY,
-         name VARCHAR(30) NOT NULL, 
-         address VARCHAR(30) NOT NULL, 
-         phone VARCHAR(20) NOT NULL, 
-         department VARCHAR(20) NOT NULL, 
-         present TINYINT(1) NOT NULL)
-         ")
-         #table - employees_pin
-         @client.query("
-           CREATE TABLE IF NOT EXISTS employees_pin(
-           pin_id INT PRIMARY KEY,
-           emp_id INT NOT NULL,
-           pin INT(4),
-           FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE ON UPDATE CASCADE )
-           ")
-          #table - employees_time
-         @client.query("
-          CREATE TABLE IF NOT EXISTS employees_time(
-            time_id INT PRIMARY KEY,
-            emp_id INT NOT NULL,
-            date DATE,
-            arrival_time DATETIME,
-            depart_time DATETIME,
-            FOREIGN KEY(emp_id) REFERENCES employees(emp_id) on delete cascade on update cascade)
-            ")
+      #  #table - employees 
+      #  @client.query("
+      #    CREATE TABLE IF NOT EXISTS employees(
+      #    emp_id INT PRIMARY KEY,
+      #    name VARCHAR(30) NOT NULL, 
+      #    address VARCHAR(30) NOT NULL, 
+      #    phone VARCHAR(20) NOT NULL, 
+      #    department VARCHAR(20) NOT NULL, 
+      #    present TINYINT(1) NOT NULL)
+      #    ")
+      #    #table - employees_pin
+      #    @client.query("
+      #      CREATE TABLE IF NOT EXISTS employees_pin(
+      #      pin_id INT PRIMARY KEY,
+      #      emp_id INT NOT NULL,
+      #      pin INT(4),
+      #      FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE ON UPDATE CASCADE )
+      #      ")
+      #     #table - employees_time
+      #    @client.query("
+      #     CREATE TABLE IF NOT EXISTS employees_time(
+      #       time_id INT PRIMARY KEY,
+      #       emp_id INT NOT NULL,
+      #       date DATE,
+      #       arrival_time DATETIME,
+      #       depart_time DATETIME,
+      #       FOREIGN KEY(emp_id) REFERENCES employees(emp_id) on delete cascade on update cascade)
+      #       ")
      rescue => e
        puts "Error connecting to database"
        puts e.message
@@ -230,6 +230,6 @@ class Attendance
     end
   end
  end #end_of_class
-
+Database.new
 a = Attendance.new
 a.account_check
--- database_connect.rb
+++ database_connect.rb
@@ -5,5 +5,37 @@ class Database
   def self.create_con
     Mysql2::Client.new(:host => "localhost", :username => "Roman", :password => "Roman123", :database => "employees_reports")
   end
-  
+  def initialize
+    @client = Mysql2::Client.new(:host => "localhost", :username => "Roman", :password => "Roman123", :database => "tests")
+    @client.query("
+      CREATE TABLE IF NOT EXISTS employees(
+      emp_id INT PRIMARY KEY,
+      name VARCHAR(30) NOT NULL, 
+      address VARCHAR(30) NOT NULL, 
+      phone VARCHAR(20) NOT NULL, 
+      department VARCHAR(20) NOT NULL, 
+      present TINYINT(1) NOT NULL)
+      ")
+      #table - employees_pin
+      @client.query("
+        CREATE TABLE IF NOT EXISTS employees_pin(
+        pin_id INT PRIMARY KEY,
+        emp_id INT NOT NULL,
+        pin INT(4),
+        FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE ON UPDATE CASCADE )
+        ")
+      #table - employees_time
+      @client.query("
+      CREATE TABLE IF NOT EXISTS employees_time(
+        time_id INT PRIMARY KEY,
+        emp_id INT NOT NULL,
+        date DATE,
+        arrival_time DATETIME,
+        depart_time DATETIME,
+        FOREIGN KEY(emp_id) REFERENCES employees(emp_id) on delete cascade on update cascade)
+        ")
+    rescue => e
+    puts "Error connecting to database"
+    puts e.message
+  end
 end
