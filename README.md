0bce381764a3fbe08e9e21fa2712c027912d73e7
kcromanpl <kcromanpl@gmail.com>
2019-09-19 06:41:38 +0545

    Pin Check Function added with looping for user input

--- README.md
+++ README.md
@@ -1,45 +1,52 @@
-**Edit a file, create a new file, and clone from Bitbucket in under 2 minutes**
+#Attendance Management System
+Attendance Management System manages the daily arrival and departure time of employees. Attendance System forms the lifeline of the business institute to manage the Employee and its salary. It is very useful for an Institute to test its employee attendance continuously for their mutual development.
 
-When you're done, you can delete the content in this README and update the file with details for others getting started with your repository.
+## Technologies used
+	Ruby | Mysql2 | pry 
 
-*We recommend that you open this README in another tab as you perform the tasks below. You can [watch our video](https://youtu.be/0ocf7u76WSo) for a full demo of all the steps in this tutorial. Open the video in a new tab to avoid leaving Bitbucket.*
+## List of Tables
 
----
+1. 	Employees
+      emp_id 	=> int Primary key,
+      Name 		=> varchar(30)
+      Address 	=> varchar(30)
+      Phone 	=> varchar(20)
+      Department => varchar(20)
+      Present 	=> TINYINT(1) n
 
-## Edit a file
+ 2. Emps_pin
 
-You’ll start by editing this README file to learn how to edit a file in Bitbucket.
+ 	pin_id		=> int Primary Key
+ 	emp_id 		=> int Foreign Key
+ 	pin 		=> int(4)
 
-1. Click **Source** on the left side.
-2. Click the README.md link from the list of files.
-3. Click the **Edit** button.
-4. Delete the following text: *Delete this line to make a change to the README from Bitbucket.*
-5. After making your change, click **Commit** and then **Commit** again in the dialog. The commit page will open and you’ll see the change you just made.
-6. Go back to the **Source** page.
+ 3. Employees_time
 
----
+ 	time_id		=> int Primary Key
+ 	Date 		=> Date
+ 	arrival_time=> TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ 	depart_time => TIMESTAMP
 
-## Create a file
+## Change Logs
 
-Next, you’ll add a new file to this repository.
+ 2.0 employees.rb - 
 
-1. Click the **New file** button at the top of the **Source** page.
-2. Give the file a filename of **contributors.txt**.
-3. Enter your name in the empty file space.
-4. Click **Commit** and then **Commit** again in the dialog.
-5. Go back to the **Source** page.
+ 1.7 update - Updates Employee records
+ 1.6 delete - Deletes Employee Records
+ 1.5 insert - Inserting Employee details
+ 1.4 list - Listing the accounts in the database
+ 1.3 access & run_again - Accessing the database if Account & Pin matches.
+ Terminates only when user decides to do so.
+ 1.2 pin_check - checks if pin matches the database
+ 1.1 account_check - checks if account is in database
+ 1.0 attendance.rb - Connection Established
 
-Before you move on, go ahead and explore the repository. You've already seen the **Source** page, but check out the **Commits**, **Branches**, and **Settings** pages.
 
----
+ 0.2 Created table Employees, emps_pin, Employees_time 
+ 0.1 Created Database - 'attnd_db'
+ 0.0 database_connect.rb - initializes the mysql2 connection
 
-## Clone a repository
 
-Use these steps to clone from SourceTree, our client for using the repository command-line free. Cloning allows you to work on your files locally. If you don't yet have SourceTree, [download and install first](https://www.sourcetreeapp.com/). If you prefer to clone from the command line, see [Clone a repository](https://confluence.atlassian.com/x/4whODQ).
+<!-- ## Installation
 
-1. You’ll see the clone button under the **Source** heading. Click that button.
-2. Now click **Check out in SourceTree**. You may need to create a SourceTree account or log in.
-3. When you see the **Clone New** dialog in SourceTree, update the destination path and name if you’d like to and then click **Clone**.
-4. Open the directory you just created to see your repository’s files.
-
-Now that you're more familiar with your Bitbucket repository, go ahead and add a new file locally. You can [push your change back to Bitbucket with SourceTree](https://confluence.atlassian.com/x/iqyBMg), or you can [add, commit,](https://confluence.atlassian.com/x/8QhODQ) and [push from the command line](https://confluence.atlassian.com/x/NQ0zDQ).
+Download the ruby file -->
--- attendance.rb
+++ attendance.rb
@@ -1,18 +1,101 @@
 require 'mysql2'
 require './database_connect'
+require './employee'
 
 class Attendance
   def initialize
-  #initializing connection
-    @client = Database.new
+    #initializes the Mysql connection 
+     begin
+       @client =   Database.create_con
+       #table - employees 
+       @client.query("
+         CREATE TABLE IF NOT EXISTS employees(
+         emp_id INT PRIMARY KEY,
+         name VARCHAR(30) NOT NULL, 
+         address VARCHAR(30) NOT NULL, 
+         phone VARCHAR(20) NOT NULL, 
+         department VARCHAR(20) NOT NULL, 
+         present TINYINT(1) NOT NULL)
+         ")
+         #table - employees_pin
+         @client.query("
+           CREATE TABLE IF NOT EXISTS employees_pin(
+           pin_id INT PRIMARY KEY,
+           emp_id INT NOT NULL,
+           pin INT(4),
+           FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE ON UPDATE CASCADE )
+           ")
+     rescue => e
+       puts "Error connecting to database"
+       puts e.message
+     end
+   end  
+
+  #terminating on user input only
+  def run_again
+    print "Continue (Y/y)? or Check Account list (A/a) : "
+    choice = gets.chomp.downcase
+    if (choice == "y")
+      access
+    elsif (choice =='a')
+      list
+        sleep(2) #program waits for 2 seconds
+      access
+    else
+      puts "TERMINATED"
+      exit
+    end
   end
 
   #checks if account is in database
   def account_check
+    #array for CRUD operation (update & delete)
+    begin
+      @result_set = @client.query("SELECT * FROM employees")
+      @update_set = Array.new
+      @result_set.each do |row|
+       @update_set<<row['emp_id']
+    end
+    rescue => e
+      puts "Account Check Failed - Database Error!"
+      puts e.message
+    end
+
     puts "\nWELCOME !!! RUBY ATTENDANCE MANAGEMENT SYSTEM !!!"
     print("Enter Your Account number: ")
-    emp_id = gets.chomp.to_i;
-    @client.employee_account_check(emp_id)
+    @emp_id = gets.chomp.to_i;
+    if (@update_set.include?(@emp_id))
+      pin_check
+    else
+      puts "Account Not Found"
+      account_check
+    end
+  end
+
+  #checks if pin matches the database
+  def pin_check
+    begin
+      @pin_set = @client.query("SELECT pin FROM employees_pin where emp_id = '#{@emp_id}' ");
+      @pin_set.each do |i|
+        @pin = i['pin'];
+      end
+    rescue => e
+      puts "Pin check failed - Database Error !!"
+      e.message
+    end
+    print ("Enter your PIN: ")
+        pin_number = gets.chomp.to_i
+        if (pin_number == @pin && @emp_id == 1)
+          print ("\nAdmin Account - PIN Matched\n")
+          access
+        elsif (pin_number == @pin)
+          print ("\nEmployee Account - PIN Matched\n")
+          # @emp.emp_choose(@pin,@emp_id)
+          puts "Employee Function"
+        else
+          print ("\nPIN : No Match\n")
+          pin_check
+        end
   end
 
   #CRUD oprations for Employee if Account & Pin Matched
@@ -31,43 +114,110 @@ class Attendance
           delete
         else
           puts "Invalid Selection"
-          # run_again
+          run_again
       end
   end
-  
+
   #listing the Employee information in the database
   def list
-    @client.list_employee
+    #array for CRUD operation (update & delete)
+    begin
+      @result_set = @client.query("SELECT * FROM employees")
+    rescue =>e
+      puts "Listing Failed - Database Error"
+      puts e.message;
+    end
+    @update_set = Array.new
+    @result_set.each do |row|
+      @update_set<<row['emp_id']
+    end
+    puts ("Employee List")
+    
+    puts "|Employee ID \t|\t Name \t|\t Address \t|\t Phone \t\t\t|\t Department \t| \t Present \t|"
+    puts "-----------------------------------------------------------------------------------------------------------------------------------------"
+    @result_set.each_with_index do |value,index|
+      print "|\t #{value['emp_id']} \t|\t #{value['name']} \t|\t #{value['address']} \t|\t #{value['phone']} \t|\t #{value['department']} \t|\t #{value['present']} \t\t|"
+     print "\n"
+    end
+    run_again
   end
-  
+
   #inserting Employee details in the information
   def insert
     puts "Enter Employee Details"
-    print "Enter Employee ID: "; emp_id = gets.chomp.to_i
-    print "Enter Name "; name = gets.chomp
-    print "Enter Address "; address = gets.chomp
-    print "Enter Phone "; phone = gets.chomp
-    print "Enter Department "; department = gets.chomp
-    print "Enter Present (0/1) "; present = gets.chomp.to_i
-    #empty check emp_id.empty?
-    @client.insert_employee(emp_id,name,address,phone,department,present);
+    print "Enter Employee ID: "; @emp_id = gets.chomp.to_i
+    print "Enter Name "; @name = gets.chomp
+    print "Enter Address "; @address = gets.chomp
+    print "Enter Phone "; @phone = gets.chomp
+    print "Enter Department "; @department = gets.chomp
+    print "Enter Present (0/1) "; @present = gets.chomp.to_i
+    begin
+      @client.query("INSERT INTO employees VALUES('#{@emp_id}','#{@name}','#{@address}','#{@phone}','#{@department}','#{@present}') ")
+      # run_again
+    rescue => e
+      puts "Insert Failed - Database Error!"
+      puts e.message
+    end
+    run_again
   end
-  
-  #Deleting Employee records
+
+  #Deleting Employee
   def delete
     puts "Deleting Employee Details"
-    print "Enter the Employee id,you want to delete: "; delete = gets.chomp.to_i
-    @client.delete_employee(delete) 
-  end
+    print "Enter the Employee id,you want to delete: "; @delete = gets.chomp.to_i
   
+    if @update_set.include?(@delete)
+      @client.query("DELETE FROM employees where emp_id = '#{@delete}'")
+      puts "Employee '#{@delete}' Deleted"
+      run_again
+    else
+      puts"Employee '#{@delete}' not found"
+      run_again
+    end
+  end
+
   #Updating Employee Information
   def update
     puts "Updating Employee Details"
-    print "Enter the Employee id,you want to update: "; update = gets.chomp.to_i
-    @client.update_employee(update)
+    print "Enter the Employee id,you want to update: "; @update = gets.chomp.to_i
+    
+    if @update_set.include?(@update)
+      print "What do You want to update ? : "
+      puts "\n1.Employee Id\n\n2.Employee Name\n\n3. Adress\n\n4. Phone Number\n\n5. Department\n\n6. Present"
+      print ("Enter Your Selection (1/2/3/4/5/6): ")
+      action = gets.chomp.to_i
+      case action
+        when 1
+          @column = "emp_id";
+        when 2
+          @column = "Name";
+        when 3
+          @column = "Address";
+        when 4
+          @column = "Phone";
+        when 5
+          @column = "Department";
+        when 6
+          @column = "Present";
+        else
+          puts "Invalid Selection"
+          run_again
+      end
+      print "Enter the New value : "; @value = gets.chomp;
+      begin
+        @client.query("UPDATE employees SET #{@column} = '#{@value}' WHERE emp_id = '#{@update}'")
+      rescue => e
+        puts "Update Failed - Database Error"
+        puts e.message
+      end
+      puts "Employee '#{@update}' Updated"
+      run_again
+    else
+      puts"Employee '#{@update}' not found"
+      run_again
+    end
   end
+ end #end_of_class
 
-
-end #end_of_class
 a = Attendance.new
 a.account_check
